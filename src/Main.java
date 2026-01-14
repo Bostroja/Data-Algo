5e25eb13734dc59c54f4a0573a9cb831c5cbb9b5
bostroja <bostroja@arcada.fi>
2026-01-14 15:39:59 +0200

    Car commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello");
+
+        System.out.println("Hello!");
+
+        Car car = new Car("Volvo");
+        Car car2 = new Car("Ford");
+
+        System.out.println(car.getName());
+        car.setName("BMW");
+        System.out.println(car.getName());
+        System.out.println(car2.getName());
     }
 
 }
